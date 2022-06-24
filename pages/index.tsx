afc8602158a9a3baa285799d9acab3159cf0c128
Etienne Lacasse <thimbledice@gmail.com>
2022-06-24 17:20:36 -0400

    Fix image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <div className='flex justify-center h-full items-center space-x-2 px-8'>
@@ -9,15 +7,11 @@ export default function Home() {
         <h1 className='text-lg mt-2'>Enseignant en techniques</h1>
         <h1 className='text-lg'>de l&apos;informatique</h1>
       </div>
-      <div className='w-96 rounded-3xl overflow-hidden'>
-        <Image
-          src='/etienne-lacasse.jpg'
-          width={2698}
-          height={2355}
-          layout='responsive'
-          alt='Photo'
-        />
-      </div>
+      <img
+        src='/etienne-lacasse.jpg'
+        className='w-96 rounded-3xl overflow-hidden'
+        alt='Picture of Étienne Lacasse'
+      />
     </div>
   )
 }
