eb9d165bb8ef66e760b4cb491e8c4f2ad9f81452
Etienne Lacasse <thimbledice@gmail.com>
2022-06-24 19:44:39 -0400

    google-site-verification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="description" content="Free Web tutorials" />
         <meta name="keywords" content="Étienne Etienne Lacasse étienne etienne lacasse enseignant" />
         <meta name="author" content="Étienne Lacasse" />
+        <meta name="google-site-verification" content="dMMR6ges2KxcGT8IjH8JtmNgbcsmWOdxlndi5_4D76k" />
       </Head>
       <div className='h-screen w-screen flex flex-col'>
         <Header />
